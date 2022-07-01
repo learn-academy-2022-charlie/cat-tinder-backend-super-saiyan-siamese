e3a5dddb37c643abefe4d797b7ebe938ac1b0b22
michael-a-tobias <michael.a.tobias@outlook.com>
2022-07-01 13:01:04 -0700

    API Endpoints MVP complete. Began to work on Update request spec, but we decided to do it later.

--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -2,6 +2,81 @@ require 'rails_helper'
 
 RSpec.describe "Cats", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of cats" do
+      Cat.create(
+        name: 'Felix',
+        age: 2,
+        enjoys: 'Walks in the park',
+        image: 'https://thiscatdoesnotexist.com/'
+      )
+
+      # Make a request
+      get '/cats'
+
+      cat = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(cat.length).to eq 1
+    end
   end
+
+  describe "POST /create" do
+    it "creates a cat" do
+      # The params we are going to send with the request
+      cat_params = {
+        cat: {
+          name: 'Buster',
+          age: 4,
+          enjoys: 'Bustin.',
+          image: 'https://thiscatdoesnotexist.com/'
+        }
+      }
+  
+      # Send the request to the server
+      post '/cats', params: cat_params
+  
+      # Assure that we get a success back
+      expect(response).to have_http_status(200)
+  
+      # Look up the cat we expect to be created in the db
+      cat = Cat.first
+  
+      # Assure that the created cat has the correct attributes
+      expect(cat.name).to eq 'Buster'
+    end
+  end
+
+  # describe "PATCH /update" do
+  #   it "updates a cat" do
+  #     # create an instance of Cat for the db
+  #     cat = Cat.create(
+  #       name: 'Felix',
+  #       age: 2,
+  #       enjoys: 'Walks in the park',
+  #       image: 'https://thiscatdoesnotexist.com/'
+  #     )
+      
+  #     # The params we are going to send with the request
+  #     cat_params = {
+  #       cat: {
+  #         name: 'Buster',
+  #         age: 4,
+  #         enjoys: 'Bustin.',
+  #         image: 'https://thiscatdoesnotexist.com/'
+  #       }
+  #     }
+  
+  #     # Send the request to the server
+  #     patch `/cats/#{cat.id}`, params: cat_params
+  
+  #     # Assure that we get a success back
+  #     expect(response).to have_http_status(200)
+  
+  #     # Look up the cat we expect to be created in the db
+  #     cat = Cat.first
+  
+  #     # Assure that the created cat has the correct attributes
+  #     expect(cat.name).to eq 'Buster'
+  #   end
+  # end
+
 end
